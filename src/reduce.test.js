86a1d46f33d08c1ff07b7617de6cd4fb1c0292cb
Mykhailo <kurochkinmy@gmail.com>
2025-11-09 12:15:06 +0100

    test: add rests for reduce method

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { reduce } = require('./reduce');
+const arr = [0, 1, 2, 3, 4];
+const sumCb = (acc, current) => acc + current;
 
 describe('reduce', () => {
   beforeAll(() => {
@@ -11,9 +13,29 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
-
+  it('should return a number', () => {
+    expect(typeof arr.reduce2(sumCb, 0)).toBe('number');
   });
 
-  // Add tests here
+  it('should return an accumulated value', () => {
+    expect(arr.reduce2(sumCb, 0)).toBe(10);
+  });
+
+  it('should return an initial value if array is empty', () => {
+    expect([].reduce2(sumCb, 0)).toBe(0);
+  });
+
+  it('should call cb for every item', () => {
+    const cb = jest.fn();
+
+    arr.reduce2(cb, 0);
+    expect(cb).toHaveBeenCalledTimes(5);
+  });
+
+  it('should call cb with all params', () => {
+    const cb = jest.fn();
+
+    arr.reduce2(cb, 0);
+    expect(cb).toHaveBeenCalledWith(0, 0, 0, arr);
+  });
 });
